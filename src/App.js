c94034ada01bf6df59b4c68ba7d6af616bfcdfe9
Bryce Perez <bryceperez31@gmail.com>
2021-11-09 13:26:45 -0700

    Search and Card components with binding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { CardList } from "./Components/card-list/card-list.component";
+import { SearchBox } from "./Components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
@@ -8,7 +9,11 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
+
+    // //Here, because all of our "this"'s are in the constructor, it knows its refering to this App class
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -18,16 +23,32 @@ class App extends Component {
       .catch((error) => console.log(error.message));
   }
 
+  //Arrow function automatically bind "this" to the class it originated from preventing us from having to bind every class method. Use Arrow function on any class methods we define that aren't part of react like render() or componentDidMount().
+  handleChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  // handleChange(event) {
+  //   // In an event, "this" is set to the element that caused it such as a button or an input field. Therefore, we need to bind this function in our constructor so it know "this" points to the App class
+  //   this.setState({ searchField: event.target.value });
+  // }
+
   render() {
+    //filteredMonsters returns a new array of monsters based on if their name includes whats in this.state.searchField. We then pass the filteredMonsters array as props instead of the complete monsters array to the CardList componenet
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        {/* //props are whats passed into the component, "name" in this case.
+        {/* //props are whats passed into the component, placeholder monsters and handleChange in this case.
         //props.children is whats passed in between the component like, <CardList>here is in between</CardList> */}
-        <CardList name="Bryce">
-          {this.state.monsters.map((monster) => (
-            <h1 key={monster.id}> {monster.name}</h1>
-          ))}
-        </CardList>
+        <SearchBox
+          placeholder="Search Monster"
+          handleChange={this.handleChange}
+          //dont call function for events, assign function to events. so this.handleChange() would call the function immediately, even when there is no onChange event where this method is used and be wrong
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
---
+++ src/Components/search-box/search-box.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./search-box.styles.css";
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input
+    className="search"
+    type="search"
+    placeholder={placeholder}
+    onChange={handleChange}
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    //   onChange={(event) =>
+    //     this.setState({ searchField: event.target.value })
+    //   }
+    // This component was originall in App.js which is why it references this.setState
+    //this.setState calls the render() method, therefore you cannot use it in the render method outside of an event or it will cause an infinite loop
+    //  {
+    // this.setState({ searchField: event.target.value }, () =>
+    //   console.log(this.state.searchField)
+    // );
+    // because this.setState is an asynchronous (javascript doesn't know how long it'll take and it doesn't happen immediately) function, when we log this.state it is always a letter behind.
+    // console.log(this.state.searchField);
+    //To fix this we add a second argument to the this.setState method which is a callback function. this tells this.setState to set the state, and then when its done run this callback function
+    // }
+    // }
+  />
+);
